9556c58494d665cb3be1ab0750dff6f3cc96bb9b
jkmilop <jkmilop@yahoo.com>
2017-05-31 12:41:26 -0500

    Update tareae1.py

--- Salvame/tareae1.py
+++ Salvame/tareae1.py
@@ -1,5 +1,10 @@
 print ("Ingrese 0 para -p o -q y cualquier otro numero para p o q\nIngrese primero p y luego q\n")
 
+t1 = "VVFF" # p
+t2 = "VFVF" # q
+t3 = "FFVV" #-p
+t4 = "FVFV" #-q
+
 
 def valorPyQ(p,q):
     p1 = False
@@ -14,12 +19,16 @@ def valorPyQ(p,q):
 
     return (s)
 
-def pvq(s):
+def negacion(s):
+
+    if s[0]==False:
+        print("F   V\n\nV   F\n")
+    else:
+        print("V   F\n\nF   V\n")
+
+def disyuncion(s):
     
-    t1 = "VVFF" # p
-    t2 = "VFVF" # q
-    t3 = "FFVV" #-p
-    t4 = "FVFV" #-q
+    
 
     tmp1 = t1
     tmp2 = t2
@@ -50,10 +59,118 @@ def pvq(s):
         print("%s\n"%t)
         j+=1
 
+def conjuncion(s):
+    
+    
 
+    tmp1 = t1
+    tmp2 = t2
+    tmp3 = ""
+
+    
+    #La t es la tabla de verdad
+    
+    if s[0]==False:
+        tmp1 = t3
+
+    if s[1]==False:
+        tmp2 = t4
+
+    i = 0
+    while i <4:
+        if tmp1[i] == "V" and tmp2[i] == "V":
+            tmp3+="V"
+        else:
+            tmp3+="F"
+        i+=1
+
+    j = 0
+    
+    while j <4:
+        t=tmp1[j]+"   "+tmp2[j]+"   "+tmp3[j]
+
+        print("%s\n"%t)
+        j+=1
+
+def condicional(s):
+    
+    
+
+    tmp1 = t1
+    tmp2 = t2
+    tmp3 = ""
+
+    
+    #La t es la tabla de verdad
+    
+    if s[0]==False:
+        tmp1 = t3
+
+    if s[1]==False:
+        tmp2 = t4
+
+    i = 0
+    while i <4:
+        if tmp1[i] == "V" and tmp2[i] == "F":
+            tmp3+="F"
+        
+        else:
+            tmp3+="V"
+        i+=1
+
+    j = 0
+    
+    while j <4:
+        t=tmp1[j]+"   "+tmp2[j]+"   "+tmp3[j]
+
+        print("%s\n"%t)
+        j+=1
+def bicondicional(s):
+    
+    
+
+    tmp1 = t1
+    tmp2 = t2
+    tmp3 = ""
+
+    
+    #La t es la tabla de verdad
+    
+    if s[0]==False:
+        tmp1 = t3
+
+    if s[1]==False:
+        tmp2 = t4
+
+    i = 0
+    while i <4:
+        if tmp1[i] == tmp2[i]:
+            tmp3+="V"
+            
+        else:
+            tmp3+="F"
+        i+=1
+
+    j = 0
+    
+    while j <4:
+        t=tmp1[j]+"   "+tmp2[j]+"   "+tmp3[j]
+
+        print("%s\n"%t)
+        j+=1
+        
 def menu():
-    s = valorPyQ(1,1)
+    s = valorPyQ(0,1)
+    
+    print(" -p\n")
+    negacion(s)
     print ("  p v q:\n")
-    pvq(s)
+    disyuncion(s)
+    print ("  p y q:\n")
+    conjuncion(s)
+    print ("  p -> q:\n")
+    condicional(s)
+    print ("  p <-> q:\n")
+    bicondicional(s)
 menu()
           
